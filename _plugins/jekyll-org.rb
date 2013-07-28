5cce7e610d964b821c66dfcd051eac6cb36e5856
vonavi <ivvl82@gmail.com>
2013-07-28 23:51:10 +0300

    "jekyll-org" plugin follows standard recommendations for plugins.

--- _plugins/jekyll-org.rb
+++ _plugins/jekyll-org.rb
@@ -1,26 +1,24 @@
 require 'org-ruby'
 
 module Jekyll
-  module Converters
-    class OrgConverter < Converter
-      safe true
-      priority :low
+  class OrgConverter < Converter
+    safe true
+    priority :low
 
-      def matches(ext)
-        ext =~ /org/i
-      end
+    def matches(ext)
+      ext =~ /org/i
+    end
 
-      def output_ext(ext)
-        ".html"
-      end
+    def output_ext(ext)
+      ".html"
+    end
 
-      def convert(content)
-        Orgmode::Parser.new(content).to_html
-      end
+    def convert(content)
+      Orgmode::Parser.new(content).to_html
     end
   end
 
-  module Filters
+  module Orgify
     # Convert an Org string into HTML output.
     #
     # input - The Org String to convert.
@@ -28,8 +26,10 @@ module Jekyll
     # Returns the HTML formatted String.
     def orgify(input)
       site = @context.registers[:site]
-      converter = site.getConverterImpl(Jekyll::Converters::OrgConverter)
+      converter = site.getConverterImpl(Jekyll::OrgConverter)
       converter.convert(input)
     end
   end
 end
+
+Liquid::Template.register_filter(Jekyll::Orgify)
