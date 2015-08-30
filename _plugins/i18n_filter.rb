b1e18a33cc8e3b2bb9a7ac1c07a190900bcb3e42
vonavi <ivvl82@gmail.com>
2015-08-30 11:45:37 +0300

    Avoid the deprecation message of I18n module.

--- _plugins/i18n_filter.rb
+++ _plugins/i18n_filter.rb
@@ -1,6 +1,8 @@
 require 'i18n'
 
 LOCALE = :ru # set your locale
+# Set this to avoid the deprecation message
+I18n.enforce_available_locales = false
 
 # Create folder "_locales" and put some locale file from
 # https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
